44559b793d2756d96470c8c2f36cf802d99c4124
joram-rogue <jorambbira@gmail.com>
2026-05-27 08:36:13 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-com.joram.nderesocial,
-  appName: 'nderesocial',
+  appId: 'com.joram.nderesocial',
+  appName: 'Ndere Social',
   webDir: 'dist',
   server: {
-    url: 'https://50d2b1f0-b38a-4148-969c-bcc0979d5337.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
-  android: {
-    backgroundColor: '#1a0f0a',
-  },
-  ios: {
-    backgroundColor: '#1a0f0a',
-    contentInset: 'always',
-  },
+    androidScheme: 'https'
+  }
 };
 
 export default config;
